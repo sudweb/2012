f37b1692176240eca2794838cb78bb3cb4cf35b9
oncletom <thomas@oncle-tom.net>
2012-01-15 19:27:37 +0100

    Queuing default less stylesheets

--- functions.php
+++ functions.php
@@ -1,5 +1,5 @@
 <?php
-
+define('THEME_VERSION', '2.0.0-dev');
 
 /*
  * Requires bundled WP-LESS for LESS stylesheet parsing features
@@ -8,4 +8,13 @@ if (false === class_exists('WPLessPlugin'))
 {
 	require __DIR__.'/vendor/wp-less/bootstrap-for-theme.php';
 	$WPLessPlugin->dispatch();
+}
+
+/*
+ * Register actions
+ */
+add_action('wp', 'theme_main_action');
+
+function theme_main_action(){
+    wp_enqueue_style('main', get_stylesheet_directory_uri('/style.less'), array(), 'THEME_VERSION', 'media,screen');
 }
