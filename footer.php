c0c86d821e28dfed766e6bb6ac38f8f3c1d66a3b
oncletom <thomas@oncle-tom.net>
2012-01-15 19:34:03 +0100

    * moved navigation in the header * moved some static parts in the template

--- footer.php
+++ footer.php
@@ -1,2 +1,17 @@
+</div>
+<footer>
+	<p>À propos de l'Association Sud-Web</p>
+	<p>Derrière Sud Web (l’événement), il y a Sud-Web (l’association). Derrière le rêve et les paillettes, il y a les travailleurs de l’ombre, quis’activent en silence pour donner vie à leur passion....</p>
+	<a href="#">Espace Presse</a>
+	<p><small>Avec le soutien du W3C</small></p>
+
+	<?php //@todo handle this with a wp_menu ?>
+	<ul>
+		<li><a href="#">Plan du site</a></li>
+		<li><a href="#">Crédits</a></li>
+		<li><a href="#">Mentions légales</a></li>
+		<ul>
+</footer>
+
 <?php wp_footer() ?>
 </body></html>
--- header.php
+++ header.php
@@ -4,3 +4,24 @@
     <?php wp_head() ?>
 </head>
 <body <?php body_class() ?>>
+
+<header>
+	<h1><?php bloginfo('title') ?></h1>
+	<p>Les 25 et 26 Mai à Toulouse, France</p>
+	<h2>Faire savoir et savoir-faire</h2>
+	<p>Sud web, c'est avant tout 2 jours d'échanges autour des technologies du web d'aujourd'hui et de demain. Notre ambition est de transmettre au plus grand nombre cette passion qui nous anime pour préserver un web ouvert de qualité.</p>
+	<figure>
+		<img src="http://farm7.staticflickr.com/6186/6070501765_5cc8d47a84_m.jpg" alt="Pardon me, I'm a newbie !" />
+	</figure>
+	<p class="btn"><a>Proposez un sujet</a></p>
+
+	<?php //@todo handle this with a wp_menu ?>
+	<nav class="unstyled">
+		<li><a href="/programme">Programme</a></li>
+		<li><a href="/partiper">Participer</a></li>
+		<li><a href="/actualites">Actualités</a></li>
+		<li><a href="/partenaires">Partenaires</a></li>
+	</nav>
+</header>
+
+<div role="main">
