7f5fa479f442c51c82133b42c039b4f2ae710193
oncletom <thomas@oncle-tom.net>
2012-01-22 19:21:38 +0100

    News listing
    refs #3

--- home.php
+++ home.php
@@ -1,3 +1,16 @@
 <?php get_header() ?>
 
+<div class="rich-list-container" id="news">
+	<h2><?php _e('Latest News', 'sudweb') ?></h2>
+
+	<?php while(have_posts()): the_post() ?>
+	<div class="rich-list-item">
+		<h3><a href="<?php the_permalink() ?> rel="bookmark"><?php the_title() ?></a></h3>
+		<?php the_post_thumbnail('post-thumbnail', array('class' => 'llustration alignright')) ?>
+		<div class="meta"><?php echo get_the_date() ?></div>
+		<p><?php the_excerpt() ?></p>
+	</div>
+	<?php endwhile ?>
+</div>
+
 <?php get_footer() ?>
