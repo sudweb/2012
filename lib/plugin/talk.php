92a37a9c9224812f79d026db6f921182077002e0
oncletom <thomas@oncle-tom.net>
2012-02-04 18:06:11 +0100

    Registering Talk Type taxonomy
    refs #4

--- lib/plugin/talk.php
+++ lib/plugin/talk.php
@@ -10,9 +10,33 @@ add_action('init', 'sudweb_register_talk');
 
 /**
  * Registers the "Talk" Custom Post Type
+ * Registers the "Talk Types" Custom Taxonomy
  */
 function sudweb_register_talk()
 {
+	/**
+	 * Registering "Talk Types"
+	 */
+	register_taxonomy('talk_types', array('talk'), array(
+		'hierarchical' => true,   //despite it's hierarchical, we expect a flat structure
+		'show_ui' => true,
+		'query_var' => true,
+		'labels' => array(
+			'name' => _x('Talk Types', 'taxonomy general name', 'sudweb'),
+			'singular_name' => _x('Talk Type', 'taxonomy singular name', 'sudweb'),
+			'search_items' =>  __('Search Talk Types', 'sudweb'),
+			'all_items' => __('All Talk Types', 'sudweb'),
+			'edit_item' => __('Edit Talk Type', 'sudweb'),
+			'update_item' => __('Update Talk Type', 'sudweb'),
+			'add_new_item' => __('Add New Talk Type', 'sudweb'),
+			'new_item_name' => __('New Talk Type Name', 'sudweb'),
+			'menu_name' => __('Talk Type', 'sudweb'),
+		),
+	));
+
+	/**
+	 * Registering "Talk"
+	 */
 	register_post_type('talk', array(
 		'labels' => array(
 			'name' => _x('Talks', 'post type general name', 'sudweb'),
@@ -39,6 +63,9 @@ function sudweb_register_talk()
 		'has_archive' => false,
 		'hierarchical' => true,
 		'menu_position' => 5, //Below posts
+		'taxonomies' => array(
+			'talk_types',
+		),
 		'supports' => array(
 			'title',
 			'editor',
