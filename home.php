4d6643222d4c479214eaec18f834ea0d9a3b8944
oncletom <thomas@oncle-tom.net>
2012-02-04 17:37:10 +0100

    Code indent
    refs #3

--- home.php
+++ home.php
@@ -1,18 +1,18 @@
 <?php get_header() ?>
 
 <div class="row">
-<div class="rich-list-container span6" id="news">
-	<h2><?php _e('Latest News', 'sudweb') ?></h2>
+	<div class="rich-list-container span6" id="news">
+		<h2><?php _e('Latest News', 'sudweb') ?></h2>
 
-	<?php while(have_posts()): the_post() ?>
-	<div class="rich-list-item">
-		<?php the_post_thumbnail('thumbnail', array('class' => 'llustration alignright')) ?>
-		<h3><a href="<?php the_permalink() ?> rel="bookmark"><?php the_title() ?></a></h3>
-		<div class="meta"><?php echo get_the_date() ?></div>
-		<p><?php the_excerpt() ?></p>
+		<?php while(have_posts()): the_post() ?>
+		<div class="rich-list-item">
+			<?php the_post_thumbnail('thumbnail', array('class' => 'llustration alignright')) ?>
+			<h3><a href="<?php the_permalink() ?> rel="bookmark"><?php the_title() ?></a></h3>
+			<div class="meta"><?php echo get_the_date() ?></div>
+			<p><?php the_excerpt() ?></p>
+		</div>
+		<?php endwhile ?>
 	</div>
-	<?php endwhile ?>
-</div>
 
 </div>
 <?php get_footer() ?>
