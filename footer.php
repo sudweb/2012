716f074b63d2733335dd6b07fd19805df9201fbc
oncletom <thomas@oncle-tom.net>
2012-02-04 18:44:48 +0100

    Reworked footer HTML structure
    refs #8

--- footer.php
+++ footer.php
@@ -1,17 +1,29 @@
 </div>
 <footer>
-	<p>À propos de l'Association Sud-Web</p>
-	<p>Derrière Sud Web (l’événement), il y a Sud-Web (l’association). Derrière le rêve et les paillettes, il y a les travailleurs de l’ombre, quis’activent en silence pour donner vie à leur passion....</p>
-	<a href="#">Espace Presse</a>
-	<p><small>Avec le soutien du W3C</small></p>
+	<div class="container">
+		<div class="row">
+			<div class="span5">
+				<p>À propos de l'Association Sud-Web</p>
+				<p>Derrière Sud Web (l’événement), il y a Sud-Web (l’association). Derrière le rêve et les paillettes, il y a les travailleurs de l’ombre, quis’activent en silence pour donner vie à leur passion....</p>
+			</div>
 
-	<?php //@todo handle this with a wp_menu ?>
-	<ul>
-		<li><a href="#">Plan du site</a></li>
-		<li><a href="#">Crédits</a></li>
-		<li><a href="#">Mentions légales</a></li>
-		<ul>
+			<div class="span3">
+				<a href="#">Espace Presse</a>
+
+				<?php //@todo handle this with a wp_menu ?>
+				<ul>
+					<li><a href="#">Plan du site</a></li>
+					<li><a href="#">Crédits</a></li>
+					<li><a href="#">Mentions légales</a></li>
+				<ul>
+			</div>
+
+			<div class="row3">
+				<p>Avec le soutien du W3C</p>
+
+				<p>Sympathiquement hébergé par AlwaysData</p>
+			</div>
+		</div>
+	</div>
 </footer>
-
-<?php wp_footer() ?>
-</body></html>
+<?php wp_footer() ?></body></html>
