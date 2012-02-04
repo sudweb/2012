ff49712b055a165c15fc8336c05964175478000c
oncletom <thomas@oncle-tom.net>
2012-02-04 18:56:21 +0100

    First part of the footer (about)
    refs #8

--- footer.php
+++ footer.php
@@ -2,8 +2,8 @@
 <footer>
 	<div class="container">
 		<div class="row">
-			<div class="span5">
-				<p>À propos de l'Association Sud-Web</p>
+			<div class="span5 alternate">
+				<h3><a href="#">À propos de l'Association Sud-Web</a></h3>
 				<p>Derrière Sud Web (l’événement), il y a Sud-Web (l’association). Derrière le rêve et les paillettes, il y a les travailleurs de l’ombre, quis’activent en silence pour donner vie à leur passion....</p>
 			</div>
 
