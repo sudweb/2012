9400075bc5d16094a2ea11c701507bfe7b392c99
oncletom <thomas@oncle-tom.net>
2012-02-05 15:17:42 +0100

    Fixing page title

--- header.php
+++ header.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html <?php language_attributes() ?>>
 <head>
-	<?php wp_title('') ?>
+	<title><?php wp_title('') ?></title>
 	<?php wp_head() ?>
 </head>
 <body <?php body_class() ?>>
