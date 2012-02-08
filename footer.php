01f425918307354a97934969fee15663b56ef722
Goulven CHAMPENOIS <goulvench@gmail.com>
2012-02-08 23:04:09 +0100

    Editing header and footer to output the necessary HTML

--- footer.php
+++ footer.php
@@ -1,21 +1,71 @@
-</div>
-<footer>
-	<div class="container">
-		<div class="row">
-			<?php dynamic_sidebar('footer') ?>
-
-			<?php wp_nav_menu(array(
-				'theme_location' => 'footer-column2',
-				'container_class' => 'span2 offset1 links',
-				'menu_class' => 'unstyled',
-			)) ?>
-
-			<?php wp_nav_menu(array(
-				'theme_location' => 'footer-column3',
-				'container_class' => 'span3 links',
-				'menu_class' => 'unstyled',
-			)) ?>
-		</div>
+	<div class="block social">
+		<h2><?php _e('Keep in touch', 'sudweb'); ?></h2>
+		<ul>
+			<li><a href="#" class="newsletter">Newsletter</a></li>
+			<li><a href="https://twitter.com/sudweb" class="twitter">Twitter</a></li>
+			<li><a href="https://www.facebook.com/SudWeb" class="facebook">Facebook</a></li>
+			<li><a href="http://lanyrd.com/2012/sudweb/" class="lanyrd">Lanyrd</a></li>
+			<li><a href="https://plus.google.com/117285954629943503768/posts" class="googleplus">Google+</a></li>
+		</ul>
 	</div>
-</footer>
-<?php wp_footer() ?></body></html>
+	<div class="block sponsors">
+		<h2><?php _e('Sponsors', 'sudweb'); ?></h2>
+		
+		<?php
+		$sponsor_types = array('gold', 'silver', 'bronze');
+		foreach ($sponsor_types as $type):
+			$items = get_posts(array(
+				'numberposts' => -1,
+				'post_type' => 'sponsor',
+				'post_status' => 'publish',
+				'sponsor_type' => $type
+			));
+			if (count ($items) == 0) {
+				continue;
+			}
+			?>
+			<ul class="<?php echo $type; ?>">
+			<?php foreach ($items as $post): setup_postdata($post); ?>
+				<?php if ($type == 'bronze'): ?>
+				<li><a href="<?php the_permalink() ?>" rel="bookmark"><?php the_title() ?></a></li>
+				<?php else: ?>
+				<li><a href="<?php the_permalink() ?>" rel="bookmark"><?php the_post_thumbnail('thumbnail', array('class' => 'illustration')) ?></a></li>
+				<?php endif ?>
+			<?php endforeach ?>
+			</ul>
+		<?php endforeach ?>
+		<p class="devenirpartenaire">Vous souhaitez devenir partenaire ? <a href="#">Téléchargez notre dossier de partenariat</a></p>
+	</div>
+</div>
+<div class="footer" role="footer">
+	<div class="container">
+		<div class="apropos">
+			<h2>À propos de l'Association Sud-Web</h2>
+			<p>Derrière Sud Web (l’événement), il y a Sud-Web (l’association). Derrière le rêve et les paillettes, il y a les travailleurs de l’ombre, qui s’activent en silence pour donner vie à leur passion....</p>
+			<?php dynamic_sidebar('footer') ?>
+		</div>
+		<?php wp_nav_menu(array(
+			'theme_location' => 'footer-column2',
+			'menu_class' => 'nav'
+		)) ?>
+		<?php wp_nav_menu(array(
+			'theme_location' => 'footer-column3',
+			'container_class' => 'span3 links',
+			'menu_class' => 'unstyled'
+		)) ?>
+		<?php /*
+		// TODO: Find a way to merge items from both menu locations into one UL 
+		<ul class="nav">
+			<li class="espacepresse"><a href="#">Espace Presse</a></li>
+			<li class="soutien-officiel"><a href="#">Avec le soutien du W3C</a></li>
+			<li><a href="#">Plan du site</a></li>
+			<li><a href="#">Crédits</a></li>
+			<li><a href="#">Mentions légales</a></li>
+			<li class="hebergement"><a href="http://www.alwaysdata.com/">Sympathiquement hébergé par AlwaysData</a></li>
+		</ul>
+		*/ ?>
+	</div>
+</div>
+<?php wp_footer() ?>
+</body>
+</html>
--- header.php
+++ header.php
@@ -1,28 +1,41 @@
 <!DOCTYPE html>
 <html <?php language_attributes() ?>>
 <head>
-	<title><?php wp_title('') ?></title>
+	<title><?php wp_title() ?></title>
+	<link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri() ?>/style.css" />
+	<meta name="HandheldFriendly" content="True">
+	<meta name="MobileOptimized" content="320">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 	<?php wp_head() ?>
 </head>
 <body <?php body_class() ?>>
+	<div class="wrapper">
+		<header class="header vevent" role="header" id="header">
+			<div class="container">
+				<h1 class="logo"><a href="<?php bloginfo('url') ?>" title="<?php bloginfo('title') ?>" rel="home"><img src="<?php echo get_stylesheet_directory_uri() ?>/assets/img/logo.png" alt="<?php bloginfo('title') ?>" /></a></h1>
+	
+				<div class="baseline"><strong><time class="dtstart" datetime="2012-05-25">25</time> et <time class="dtstart" datetime="2012-05-25">26 mai 2012</time></strong> à <span class="location">Toulouse, France</span></div>
+		
+				<a href="#" class="cta beforepitch">Proposez un sujet !</a>
 
-<header id="header">
-	<div class="container">
-
-	<h1><a href="<?php bloginfo('url') ?>" title="<?php bloginfo('title') ?>" rel="home"><img src="<?php echo get_stylesheet_directory_uri() ?>/assets/img/logo.png" alt="<?php bloginfo('title') ?>" /></a></h1>
-
-		<div class="baseline"><strong><time class="dtstart" datetime="2012-05-25">25</time> et <time class="dtstart" datetime="2012-05-25">26 mai 2012</time></strong> à <span class="location">Toulouse, France</span></div>
-
-		<?php dynamic_sidebar('header') ?>
-
-		<p class="aligncenter"><a class="cta top-right" href="#">Proposer un sujet !</a></p>
-
-		<?php wp_nav_menu(array(
-			'theme_location' => 'header',
-			'container' => 'nav',
-			'menu_class' => 'row',
-		)) ?>
-	</div>
-</header>
+				<div class="pitch">
+					<h2>Faire-savoir et savoir-faire</h2>
+					<p>Sud web, c'est avant tout 2 jours d'échanges autour des technologies du web d'aujourd'hui et de demain. Notre ambition est de transmettre au plus grand nombre cette passion qui nous anime pour préserver un web ouvert de qualité.</p>
+				</div>
+				<div class="illustration">
+					<img src="<?php echo get_stylesheet_directory_uri() ?>/assets/img/placeholder.png" alt="" />
+					<?php dynamic_sidebar('header') ?>
+				</div>
+			</div>
+			<div class="nav">
+			<?php wp_nav_menu(array(
+				'theme_location' => 'header',
+				'menu_class' => 'container',
+			)) ?>
+			</div>
+		</div>
+	</header>
+</div>
 
 <div class="container" role="main" id="content">
+	<a href="#" class="cta afterpitch">Proposez un sujet !</a>
