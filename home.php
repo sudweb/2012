037e5d1bf84a71cdcbf6b7b63576f92aed490cf2
Goulven CHAMPENOIS <goulvench@gmail.com>
2012-02-08 23:23:44 +0100

    Editing page content loops to generate the expected HTML

--- home.php
+++ home.php
@@ -1,36 +1,40 @@
 <?php get_header() ?>
 
-<div class="row">
-	<div class="rich-list-container span6" id="news">
-		<h2><?php _e('Latest News', 'sudweb') ?></h2>
+<div class="block actualites">
+	<h2><?php _e('Latest News', 'sudweb') ?></h2>
 
-		<?php while(have_posts()): the_post() ?>
-		<div class="rich-list-item">
-			<?php the_post_thumbnail('thumbnail', array('class' => 'illustration alignright')) ?>
-			<h3><a href="<?php the_permalink() ?>" rel="bookmark"><?php the_title() ?></a></h3>
-			<div class="meta"><?php echo get_the_date() ?></div>
-			<p><?php the_excerpt() ?></p>
-		</div>
-		<?php endwhile ?>
+	<?php while(have_posts()): the_post() ?>
+	<div>
+		<?php the_post_thumbnail('thumbnail', array('class' => 'illustration')) ?>
+		<h3><a href="<?php the_permalink() ?>" rel="bookmark"><?php the_title() ?></a></h3>
+		<div class="meta"><?php echo get_the_date() ?></div>
+		<p><?php the_excerpt() ?></p>
 	</div>
-
-	<div class="rich-list-container span6" id="talk">
-		<h2><?php _e('Highlighted Talk', 'sudweb') ?></h2>
-
-		<?php $talks = get_posts(array(
-			'numberposts' => 1,
-			'orderby' => 'rand',
-			'post_type' => 'talk',
-			'post_status' => 'publish',
-		)) ?>
-		<?php foreach($talks as $post): setup_postdata($post) ?>
-		<div class="rich-list-item">
-			<?php the_post_thumbnail('thumbnail', array('class' => 'illustration alignright')) ?>
-			<h3><a href="<?php the_permalink() ?>" rel="bookmark"><?php the_title() ?></a></h3>
-			<div class="meta"><?php _e('By', 'sudweb') ?>&nbsp;<?php the_author_posts_link() ?></div>
-			<p><?php the_excerpt() ?></p>
-		</div>
-		<?php endforeach ?>
-	</div>
+	<?php endwhile ?>
 </div>
+
+<div class="block programme">
+	<h2><?php _e('Highlighted Talk', 'sudweb') ?></h2>
+
+	<?php $talks = get_posts(array(
+		'numberposts' => 1,
+		'orderby' => 'rand',
+		'post_type' => 'talk',
+		'post_status' => 'publish',
+	)) ?>
+	<?php foreach($talks as $post): setup_postdata($post) ?>
+	<div>
+		<?php the_post_thumbnail('thumbnail', array('class' => 'illustration')) ?>
+		<h3><a href="<?php the_permalink() ?>" rel="bookmark"><?php the_title() ?></a></h3>
+		<div class="meta"><?php _e('By', 'sudweb') ?> <?php the_author_posts_link() ?></div>
+		<p><?php the_excerpt() ?></p>
+	</div>
+	<?php endforeach ?>
+	<?php if (count ($talks) == 0): ?>
+	<div>
+		<p>Le programme n'est pas encore disponible, suivez-nous pour être informé dès qu'il sera annoncé !</p>
+	</div>
+	<?php endif ?>
+</div>
+
 <?php get_footer() ?>
