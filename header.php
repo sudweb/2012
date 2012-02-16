de505948230da65559333e280429e17a6e20a1f1
Goulven CHAMPENOIS <goulvench@gmail.com>
2012-02-16 03:36:40 +0100

    Indicating the sidebar we want to display

--- header.php
+++ header.php
@@ -38,5 +38,5 @@
 <div class="container" role="main" id="content">
 	<a href="#" class="cta afterpitch">Proposez un sujet !</a>
 	<?php if (!is_front_page()): ?>
-		<?php get_sidebar(); ?>
+		<?php get_sidebar('conferences'); ?>
 	<?php endif; ?>
