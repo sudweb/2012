18fc7377bf9bdb52d49e50f70b1e338fdad0b7b9
oncletom <thomas@oncle-tom.net>
2012-02-26 22:41:24 +0100

    Preliminary list of related talks

--- single-speaker.php
+++ single-speaker.php
@@ -12,8 +12,11 @@
 			<?php the_content() ?>
 
 			<?php $talks = p2p_type('talk_to_speaker')->get_connected(get_the_id()) ?>
-			<?php /* @todo do something working with `locate_template` */ ?>
-			<?php p2p_list_posts($talks) ?>
+			<?php p2p_list_posts($talks, array(
+				'template' => 'single-talk-embed.php',
+				'before_list' => '', 'after_list' => '',
+				'before_item' => '', 'after_item' => '',
+			)) ?>
 		</article>
 	</div>
 </div>
