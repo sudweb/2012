873db70f295ed8b5c2b87007e387f86839c11bb1
oncletom <thomas@oncle-tom.net>
2012-02-26 19:09:35 +0100

    Typo

--- lib/plugin/schedule.php
+++ lib/plugin/schedule.php
@@ -20,7 +20,7 @@ function sudweb_register_schedule()
 		'labels' => array(
 			'name' => _x('Days', 'post type general name', 'sudweb'),
 			'singular_name' => _x('Day', 'post type singular name', 'sudweb'),
-			'add_new' => __('Add New', 'talk'),
+			'add_new' => __('Add New', 'schedule'),
 			'add_new_item' => __('Add New Day', 'sudweb'),
 			'edit_item' => __('Edit Day', 'sudweb'),
 			'new_item' => __('New Day', 'sudweb'),
