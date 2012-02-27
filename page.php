63f5b688d3a42eb518972d198c382706541c4553
oncletom <thomas@oncle-tom.net>
2012-02-27 12:53:24 +0100

    Preliminary page layout

--- page.php
+++ page.php
@@ -1,3 +1,18 @@
-<?php get_header() ?>
-<?php get_template_part( 'loop', 'page' ) ?>
+<?php get_header(); the_post(); ?>
+
+<div class="row">
+	<?php get_sidebar('conferences') ?>
+
+	<div class="span9">
+		<article <?php post_class() ?>>
+			<header>
+				<?php the_post_thumbnail() ?>
+				<h2 class="conference-title"><?php the_title() ?></h2>
+			</header>
+
+			<?php the_content() ?>
+		</article>
+	</div>
+</div>
+
 <?php get_footer() ?>
