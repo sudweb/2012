1517b91293b2a871bc16d678d6ddf02034eb1dcc
oncletom <thomas@oncle-tom.net>
2012-02-29 17:24:01 +0100

    Added language field And code reorg
    refs #23

--- single-talk.php
+++ single-talk.php
@@ -3,15 +3,13 @@
 <div class="row">
 	<?php get_sidebar('conferences') ?>
 
-	<?php $speakers = p2p_type('talk_to_speaker')->get_connected(get_the_id()) ?>
-	<?php $schedule = p2p_type('talk_to_schedule')->get_connected(get_the_id())->next_post() ?>
-
 	<div class="content">
 		<article <?php post_class() ?>>
 			<header>
 				<?php the_post_thumbnail() ?>
 				<h2 class="post-title"><?php the_title() ?></h2>
 				<?php if (function_exists('p2p_list_posts')): ?>
+				<?php $speakers = p2p_type('talk_to_speaker')->get_connected(get_the_id()) ?>
 				<?php p2p_list_posts($speakers, array(
 					'before_list' => '<span class="post-speaker">',
 					'after_list' => '</span>',
@@ -21,12 +19,23 @@
 				<?php endif ?>
 			</header>
 
+			<?php if (function_exists('p2p_list_posts')): ?>
+			<?php $schedule = p2p_type('talk_to_schedule')->get_connected(get_the_id())->next_post() ?>
+			<p>
 				<span class="post-datetime">
 					<a href="<?php echo get_post_permalink($schedule->ID) ?>"><?php echo $schedule->post_title ?></a>
 					à <?php the_field('schedule') ?>
 				</span>
+
+				<?php if (function_exists('get_the_terms')): ?>
 				(<?php echo get_the_terms(get_the_id(), 'talk_types')->name ?>)
+				<?php endif ?>
+
+				<?php if (get_field('language')): ?>
+				– <span class="language"><?php the_field('language') ?></span>
+				<?php endif ?>
 			</p>
+			<?php endif ?>
 
 			<?php the_content() ?>
 		</article>
