9f2cd25939e632c3b30f967acfddb235bb5b0337
Goulven CHAMPENOIS <goulvench@gmail.com>
2012-03-02 23:57:34 +0100

    Fixing page title level (h2 to h1), fixes #51

--- page-article.php
+++ page-article.php
@@ -1,7 +1,7 @@
 <article <?php post_class() ?>>
 	<header>
 		<?php if (has_post_thumbnail()): the_post_thumbnail(); endif ?>
-		<h2 class="post-title"><a href="<?php the_permalink() ?>" rel="bookmark"><?php the_title() ?></a></h2>
+		<h1 class="post-title"><a href="<?php the_permalink() ?>" rel="bookmark"><?php the_title() ?></a></h1>
 	</header>
 
 	<?php the_content() ?>
