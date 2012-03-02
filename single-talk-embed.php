5bb2c708095e3b1618f0f5eb3641453d3bc72193
oncletom <thomas@oncle-tom.net>
2012-03-02 12:14:39 +0100

    Speaker is not displayed on a speaker page
    refs #33

--- single-talk-embed.php
+++ single-talk-embed.php
@@ -1,3 +1,4 @@
+<?php global $wp_query ?>
 <div class="schedule-item schedule-talk">
 	<?php the_post_thumbnail('tiny-thumbnail') ?>
 	<h3 class="schedule-item-title"><a href="<?php the_permalink() ?>" rel="bookmark"><?php the_title() ?></a></h3>
@@ -8,7 +9,9 @@
 	</div>
 	<?php endif ?>
 
+	<?php if (get_post_type($wp_query->get_queried_object()) !== 'speaker'): ?>
 	<div class="schedule-item-subtitle">
 		Animé par <?php sudweb_list_speakers(get_the_id()) ?>
 	</div>
+	<?php endif ?>
 </div>
