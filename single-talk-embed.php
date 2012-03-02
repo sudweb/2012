38ceaf751cae835f17e03e811690f0ce3087d1a1
oncletom <thomas@oncle-tom.net>
2012-03-02 12:06:48 +0100

    Schedule is not mandatory Display is now on demand
    refs #33

--- single-talk-embed.php
+++ single-talk-embed.php
@@ -2,9 +2,11 @@
 	<?php the_post_thumbnail('tiny-thumbnail') ?>
 	<h3 class="schedule-item-title"><a href="<?php the_permalink() ?>" rel="bookmark"><?php the_title() ?></a></h3>
 
+	<?php if (get_field('schedule')): ?>
 	<div class="schedule-item-timeframe">
 		<?php the_field('schedule') ?>
 	</div>
+	<?php endif ?>
 
 	<div class="schedule-item-subtitle">
 		Animé par <?php sudweb_list_speakers(get_the_id()) ?>
