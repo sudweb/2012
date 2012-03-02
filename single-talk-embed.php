d342ebd955fdedb8c371694203d1b69adae88c22
oncletom <thomas@oncle-tom.net>
2012-03-02 15:53:25 +0100

    Removed call to `$wp_query`
    refs #33

--- single-talk-embed.php
+++ single-talk-embed.php
@@ -1,17 +1,24 @@
-<?php global $wp_query ?>
 <div class="schedule-item schedule-talk">
 	<?php the_post_thumbnail('tiny-thumbnail') ?>
 	<h3 class="schedule-item-title"><a href="<?php the_permalink() ?>" rel="bookmark"><?php the_title() ?></a></h3>
 
-	<?php if (get_field('schedule')): ?>
+	<!-- Timeframe Content -->
+	<?php if (get_post_type(get_queried_object()) === 'speaker'): ?>
+	<div class="schedule-item-timeframe">
+
+	</div>
+	<?php elseif (get_field('schedule')): ?>
 	<div class="schedule-item-timeframe">
 		<?php the_field('schedule') ?>
 	</div>
 	<?php endif ?>
 
-	<?php if (get_post_type($wp_query->get_queried_object()) !== 'speaker'): ?>
+	<!-- Subtitle -->
 	<div class="schedule-item-subtitle">
+		<?php if (get_post_type(get_queried_object()) !== 'speaker'): ?>
 		Animé par <?php sudweb_list_speakers(get_the_id()) ?>
+		<?php else: ?>
+		<?php echo get_the_terms(get_the_id(), 'talk_types')->name ?>
+		<?php endif ?>
 	</div>
-	<?php endif ?>
 </div>
