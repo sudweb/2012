3452c33d2c105e7e9d8d5f2c9aa902734a15ed59
oncletom <thomas@oncle-tom.net>
2012-03-02 10:32:30 +0100

    Base template for scheduled day
    refs #33

---
+++ single-schedule.php
@@ -0,0 +1,13 @@
+<?php get_header(); the_post() ?>
+
+<div class="row">
+	<?php get_sidebar('conferences') ?>
+
+	<div class="content">
+		<?php get_template_part('page', 'article') ?>
+
+
+	</div>
+</div>
+
+<?php get_footer() ?>
