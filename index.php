326dfec522c49adc0c3d5d81c324244b04ab372d
oncletom <thomas@oncle-tom.net>
2012-03-06 10:17:42 -0600

    Sidebar switcher
    refs #65

--- index.php
+++ index.php
@@ -1,7 +1,12 @@
 <?php get_header(); the_post(); ?>
 
 <div class="row">
+	<?php $involvement_page = get_page_by_title('Participer') ?>
+	<?php if ($involvement_page && ($involvement_page->ID === get_the_ID() || in_array($involvement_page->ID, get_ancestors(get_the_ID(), 'page')))): ?>
+	<?php get_sidebar('attending') ?>
+	<?php else: ?>
 	<?php get_sidebar('conferences') ?>
+	<?php endif ?>
 
 	<div class="content">
 		<?php get_template_part('page', 'article') ?>
