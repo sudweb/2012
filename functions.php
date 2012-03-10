5332aa554574c607b80ef5f81a3b5fe6e16b050d
Frank Taillandier <frank.taillandier@gmail.com>
2012-03-10 22:59:37 +0100

    add a new image size for silver sponsors

--- functions.php
+++ functions.php
@@ -5,6 +5,7 @@ load_theme_textdomain('sudweb', get_template_directory().'/i18n');
 set_post_thumbnail_size(870, 220, true);
 add_image_size('tiny-thumbnail', 60, 60, true);
 add_image_size('sponsor-logo', 150, 150, false);
+add_image_size('sponsor-silver', 100, 100, false);
 
 /*
  * Requires bundled WP-LESS for LESS stylesheet parsing features
