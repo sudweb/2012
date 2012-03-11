83d2e80aa453a3ce1b7bc216549dcd858461708d
Goulven Champenois <goulvench@gmail.com>
2012-03-11 23:27:37 +0100

    Changing H3 to H2, fixes #94

--- schedule.php
+++ schedule.php
@@ -11,12 +11,12 @@ Template Name: Programme
 		<ol class="schedule-list">
 		<?php foreach (get_posts('post_type=schedule&orderby=menu_order&order=ASC') as $post): setup_postdata($post); ?>
 			<li class="schedule-item schedule-day vevent">
-				<h3 class="schedule-item-title">
+				<h2 class="schedule-item-title">
 					<a href="<?php the_permalink() ?>" rel="bookmark">
 						<?php the_post_thumbnail('thumbnail', array('alt' => '')) ?>
 						<?php the_title() ?>
 					</a>
-				</h3>
+				</h2>
 				<div class="schedule-item-content"><?php the_excerpt() ?></div>
 			</li>
 		<?php endforeach ?>
